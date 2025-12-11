0647fada0e0412872784a51ee95d44e4948a05ef
thomas-loeffler <tommyryan2004@gmail.com>
2025-12-10 23:00:55 -0500

    finished blinky and started I2C functions

--- blink/blink.c
+++ blink/blink.c
@@ -6,37 +6,57 @@
 
  // I think this is the pico SDK library that has all the shortcuts
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
 
 
-#define LED_DELAY_MS 250
 
-
-// Perform initialisation
-void pico_led_init(void) {
-    // GP 25 is the on-board LED for Pico 2
-    gpio_init(25);
-    // setting data direction of the LED to output
+// Perform initialization
+void led_init(void) {
+    // GP 25 is the on-board LED for the Pico 2
+    // set_dir sets the pin to be as an input or output
     gpio_set_dir(25, GPIO_OUT);
+    // put sets the pin to high or low
+    gpio_put(25, 0);
+    // set_function sets the pin to be used as a GPIO pin (SIO = synchronized input/output)
+    gpio_set_function(25, GPIO_FUNC_SIO);
 }
 
-// Turn the led on or off
-void pico_set_led(bool led_on) {
-#if defined(PICO_DEFAULT_LED_PIN)
-    // Just set the GPIO on or off
-    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-    // Ask the wifi "driver" to set the GPIO on or off
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
-#endif
+void i2c_setup(void){
+    // Use i2c0, 100 kHz speed
+    i2c_init(i2c0, 100 * 1000);
+
+    // Set up the GPIO pins to the I2C function
+    gpio_set_function(0, GPIO_FUNC_I2C);   // SDA
+    gpio_set_function(1, GPIO_FUNC_I2C);   // SCL
+
+    // Enable pull-ups (typical for I2C)
+    gpio_pull_up(0);
+    gpio_pull_up(1);
 }
+
+
+
+void blinky(void){
+    gpio_put(25, true);
+    sleep_ms(200);
+    gpio_put(25, false);
+    sleep_ms(200);
+    gpio_put(25, true);
+    sleep_ms(200);
+    gpio_put(25, false);
+    sleep_ms(200);
+    gpio_put(25, true);
+    sleep_ms(200);
+    gpio_put(25, false);
+    sleep_ms(5000);
+}
+
+
 
 int main() {
-    pico_led_init();
+    led_init();
 
-    while (true) {
-        pico_set_led(true);
-        sleep_ms(LED_DELAY_MS);
-        pico_set_led(false);
-        sleep_ms(LED_DELAY_MS);
+    while (1) {
+        blinky();
     }
 }
